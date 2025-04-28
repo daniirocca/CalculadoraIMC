ea8c969aafa847e58d5c1b10e57b1dcb0c710075
daniirocca <117545155+daniirocca@users.noreply.github.com>
2025-04-27 21:09:30 -0300

    add language selector and UI improvements with JSON translations

--- Services/ITranslator.cs
+++ Services/ITranslator.cs
@@ -2,5 +2,7 @@
 {
     public interface ITranslator
     {
+        string this[string key] { get; }   // Para pegar a tradução pela chave
+        void SetCulture(string culture);   // Para mudar a cultura
     }
 }
